0caa86eb19e9fd3d6117c6623ffdac5afa10c5fb
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-10-14 10:44:30 +0000

    Added base template and allauth templates

--- urbanflow/settings.py
+++ urbanflow/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    'django.contrib.sites', # Required for allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
